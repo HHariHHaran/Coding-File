c16d507f7c63ab3a31f3194e14c9bad5cb629677
HHariHHaran <43338358+HHariHHaran@users.noreply.github.com>
2018-09-17 15:51:29 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter the two numbers to add\n");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("Ans is: %d".&c);
+getch();
+}
