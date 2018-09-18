601589eaa4717020afeb4ebb9a516c6cc8d0d9b5
HHariHHaran <43338358+HHariHHaran@users.noreply.github.com>
2018-09-18 14:02:11 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,11 @@
+a=int(input("enter the number 1:"))
+b=int(input("enter the number 2:"))
+c=int(input("enter the number 3:"))
+if(a>b)(a>c)
+ print("a is greater:")
+elseif(b>c)
+ print("b is greater:")
+else
+print(c)
+ 
+ 
